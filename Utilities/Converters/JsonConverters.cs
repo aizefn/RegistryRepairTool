dd4518dbf899cfc079874537dcc412011e8e58b9
artem <artem@ARTEM-PC>
2025-05-22 19:52:20 +0300

    NewSettings

---
+++ Utilities/Converters/JsonConverters.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RegistryRepairTool.Utilities
+{
+    public class RelayCommandConverter : JsonConverter<RelayCommand>
+    {
+        public override RelayCommand Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return null; // Команды не сериализуем
+        }
+
+        public override void Write(Utf8JsonWriter writer, RelayCommand value, JsonSerializerOptions options)
+        {
+            writer.WriteNullValue(); // Команды не сериализуем
+        }
+    }
+}
